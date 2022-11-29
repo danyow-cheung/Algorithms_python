0d25aa3aef510aedd3c2f0f1c7321ad4438f8337
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2022-11-29 22:57:48 +0800

    upload

---
+++ leetcode/dp_509.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<=1:
+            return n
+        else:
+            return self.fib(n-1)+self.fib(n-2)
+
+
+n = 2
+obj = Solution().fib(n)
