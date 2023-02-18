01ade37908a6198b3c86d5a8afa6a82c6351738f
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2023-02-18 18:58:54 +0800

    2.18

--- leetcode/DivideandConquer_190.py
+++ leetcode/DivideandConquer_190.py
@@ -8,3 +8,11 @@ class Solution:
             n = ~(n-1)
             ans += 1
         return ans
+
+    def reverserBits_leetcode(self,n):
+        res = 0
+        for _ in range(32):
+            res = (res<<1)+(n&1)
+            n>>=1
+        return res 
+         
--- leetcode/stack_84.py
+++ leetcode/stack_84.py
@@ -4,3 +4,19 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
+        heights.append(0)
+        stack = [-1]
+        ans = 0
+        for i in xrange(len(heights)):
+            while heights[i]<heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i-stack[-1]-1
+                ans = max(ans,h*w)
+            stack.append(i)
+        heights.pop()
+        return ans 
+        
+
+
+heights = [2,1,5,6,2,3]
+obj = Solution().largestRectangleArea(heights)
